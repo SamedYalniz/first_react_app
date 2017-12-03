fa02c50bf19057ea09446747f04d4f756f6c897d
Samed Yalniz <s.yalniz@hotmail.de>
2017-12-03 19:42:40 +0000

    add propType checks

--- app/js/components/NameList.js
+++ app/js/components/NameList.js
@@ -9,15 +9,16 @@ export default class NameList extends React.Component {
 		}
 
 		render(){
-			const nameArray = ["Chris", "Johan", "Billybob", "Joeyjoe", "Tomas"];
+			const nameArray = ["Chrisas", "Johans", "Billybobs", "Joeyjoe", "Tomass"];
+			const element = <p> I'm an element </p>
 			return (
 				<div> 
-					<h3> I am number {this.props.number}! </h3>
-					<Welcome firstName ="Chris" /> 
+					<h3> I am number {this.props.number}!  </h3>
+					<Welcome firstName ="Chris" element={element} /> 
 					<ul>
 		 				{this.nameMapper(nameArray)}
 					</ul>
-					<Welcome thisfirstName= "Jane" lastName="Doe" />
+					<Welcome thisfirstName= "Jane" lastName="Doeese" />
 				</div>
 				)
 		}	
--- app/js/components/Welcome.js
+++ app/js/components/Welcome.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class Welcome extends React.Component {
 	render(){
@@ -6,7 +7,35 @@ export default class Welcome extends React.Component {
 		<div>
 	<h4>Welcome to my site, {this.props.firstName}! </h4>
 	<h4> Content: {this.props.lastName}! </h4>
+	{this.props.element}
 	</div>
 	)
 	}
+}
+
+const lengthChecker = ((props, propName, component) => {
+	if (props[propName]) {
+		let val = props[propName]
+			if (typeof val === "string") {
+				return val.length >= 5 ? null : new Error(`${propName} in ${component} is too short`)             //(propName +" in" + component + "is too short")
+			}
+	}
+	return null;
+
+
+})
+
+Welcome.propTypes = {
+	firstName: PropTypes.string.isRequired,
+	lastName: lengthChecker,
+	element: PropTypes.oneOfType([
+		PropTypes.element, 
+		PropTypes.string,
+	])
+
+}
+
+Welcome.defaultProps = {
+	firstName: "First Name",
+	lastName: "Last Name",
 }
